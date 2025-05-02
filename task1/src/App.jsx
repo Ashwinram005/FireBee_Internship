7ec60365fea968fa584312d33ecd873fbc591d5a
Ashwinram005 <ashwinrammohan2005@gmail.com>
2025-05-02 16:22:23 +0530

    updated

--- task1/src/App.jsx
+++ task1/src/App.jsx
@@ -1,9 +1,5 @@
 import { useEffect, useState } from "react";
-import { useEffect, useState } from "react";
 import biticon from "./assets/bitexelicon.jpg";
-import png from "./assets/png-removebg-preview.png";
-import { CiLight } from "react-icons/ci";
-import { FiMessageCircle } from "react-icons/fi";
 import png from "./assets/png-removebg-preview.png";
 import { CiLight } from "react-icons/ci";
 import { FiMessageCircle } from "react-icons/fi";
@@ -21,7 +17,6 @@ const schema = z.object({
   password: z
     .string()
     .nonempty("Password is required")
-    .min(6, "Password must be at least 6 characters"), // Password length validation
     .min(6, "Password must be at least 6 characters"), // Password length validation
 });
 
@@ -45,7 +40,10 @@ const signupSchema = z
 function AuthPage() {
   const [formType, setFormType] = useState("login"); // State to manage form type
   const navigate = useNavigate();
-
+  const token = localStorage.getItem("authToken");
+  if(token){
+    navigate("/dashboard")
+  }
   const form = useForm({
     resolver: zodResolver(formType === "login" ? schema : signupSchema),
   });
@@ -292,9 +290,6 @@ function AuthPage() {
               <p className={`${isDarkMode}:"text-gray-500":"text-white"`}>
                 Forget Password?
               </p>
-              <p className={`${isDarkMode}:"text-gray-500":"text-white"`}>
-                Forget Password?
-              </p>
             </div>
             <button
               type="submit"
@@ -318,7 +313,6 @@ function AuthPage() {
           </form>
         </div>
       </div>
-      <div className="flex justify-center bg-black absolute bottom-0 w-full">
       <div className="flex justify-center bg-black absolute bottom-0 w-full">
         <p className="text-white">@2025 Bitexel copyrights</p>
       </div>
